47f1e453d7d206c713d0c88c3d4058f99980d72e
James <james77@gmail.com>
2016-03-16 02:53:43 +0000

    test

--- nodeapp/recipes/default.rb
+++ nodeapp/recipes/default.rb
@@ -18,8 +18,8 @@ application app_path do
 
 
   Chef::Log.info("********** The app's initial state is '#{node['state']}' **********")
-  Chef::Log.info("ENV: #{node[:deploy]['simplephpapp'][:environment_variables]}")
-  Chef::Log.info("USER_ID: #{node[:deploy]['simplephpapp'][:environment_variables][:NODE_PATH]}")
+  Chef::Log.info("ENV: #{app[:deploy]['simplephpapp'][:environment_variables]}")
+  Chef::Log.info("USER_ID: #{app[:deploy]['simplephpapp'][:environment_variables][:NODE_PATH]}")
 
   file "/tmp/git_wrapper.sh" do
     owner "root"
